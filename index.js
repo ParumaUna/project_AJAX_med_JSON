629f6731b4ebb98de13e29112a44f7ce0b378df2
ParumaUna <una.paruma10@gmail.com>
2023-01-15 01:29:20 +0100

    simple-movie-display-done

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const mainBody = document.getElementById('mainBody');
-const movie = document.getElementById('movie-input');
+const searchbar = document.getElementById('movie-input');
 const click = document.getElementById('butn');
-console.log(movie.value);
+const titleDiv =document.getElementById('title')
+console.log(searchbar.value);
 
-click.addEventListener('click',async function(){
+click.addEventListener('click',async function(e){
+    e.preventDefault;
 
     try {
-        const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c51128ab&s='+ movie.value);
+        const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c51128ab&s='+ searchbar.value);
         if(response.ok === false) {
             throw new Error(`HTTP error code: ${response.status}, HTTP error message: ${response.statusText}`);
         } 
         const data = await response.json();
-        console.log(data)
-        document.getElementById('mainBody').innerHTML = data;
+        console.log(data);
+
+
+        let movies = data.Search;
+        for(let movie of movies){
+            console.log(movie);
+            postsHTML= "";
+            titleDiv.innerHTML += `
+            <div>
+                <h2>${movie.Title}</h2>
+            </div>
+            <div>
+                <img src="${movie.Poster}"></img>
+            </div>
+            <div>
+                <p>${movie.Year}</p>
+            </div>
+            
+            `
+            console.log(movie.Poster);
         
+        }
+
     } catch (error) {
         
         console.log(error)
@@ -21,7 +42,7 @@ click.addEventListener('click',async function(){
     } finally {
         console.log('Will alwase run at the end, no matter the outcome of the request')
     }
-
+    
 })
 
 fetchData();
